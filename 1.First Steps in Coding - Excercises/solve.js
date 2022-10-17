b52e591bbe582a48571ac53b4dcbce6e8691fed1
djaniAntova <djaniantova@gmail.com>
2022-10-17 23:28:16 +0300

    Delete some unnecessary files

--- 1.First Steps in Coding - Excercises/solve.js
+++
@@ -1,5 +0,0 @@
-function solve (input) {
-    console.table(input)
-
-}
-solve(["3.1416", "23243", 432424, "e3r65464"])
--- 1.First Steps in Coding - Excercises/test.js
+++
@@ -1,8 +0,0 @@
-function test(input) {
-    let i = 0;
-while (i < 3) { // shows 0, then 1, then 2
-  console.log(i);
-  i++;
-}
-}
-test(30)
