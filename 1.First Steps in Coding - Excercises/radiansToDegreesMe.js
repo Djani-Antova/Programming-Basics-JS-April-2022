e86d8ed7b4fd7ad06451cbcabea2e2256dbed514
djaniAntova <djaniantova@gmail.com>
2022-10-17 23:26:44 +0300

    Update

--- 1.First Steps in Coding - Excercises/radiansToDegreesMe.js
+++
@@ -1,8 +0,0 @@
-function radiansToDegreesMe (input) {
-    let radians = Number(input[0]);
-    let degree = radians * 180 / Math.PI;
-
-    console.log(degree)
-
-} 
-radiansToDegreesMe(["6.2832"]);
