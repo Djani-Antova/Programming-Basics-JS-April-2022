21b90225e4644f249670e506f4b6539d1d170a6a
djaniAntova <djaniantova@gmail.com>
2022-11-26 10:17:34 +0200

    Add task solutions
    Add task solutions for: Exam Preparation

---
+++ Exam Solutions - Match 2020/energy Booster.js
@@ -0,0 +1,22 @@
+function energyBooster(input) {
+    let fruit = input[0];
+    let size = input[1];
+    let count = Number(input[2]);
+    
+    switch (size) {
+        case "small":
+            
+            
+            break;
+    
+        default:
+            break;
+    }
+
+
+    
+}
+energyBooster (["Watermelon",
+    "big",
+    "4"
+    ])
